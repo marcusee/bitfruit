b6f4c6e805bf276017279e86b256f754b896754c
marcus <marcuseecl@gmail.com>
2021-12-12 17:02:37 +0800

    Add Fruit struct

--- contracts/Bitfruit.sol
+++ contracts/Bitfruit.sol
@@ -9,11 +9,55 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "hardhat/console.sol";
 
 contract Bitfruit is ERC721URIStorage {
+    using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address public owner;
+
+    mapping(uint256 => Fruit) tokenIdToFruit;
+
+    struct Fruit {
+      uint256 tokenId;
+      string data;
+      uint256 created;
+    }
 
     constructor() ERC721("BitFruit", "BFT") {
       owner = payable(msg.sender);
     }
 
+    function createFruit(string memory data) public returns (uint256) {
+        uint256 newTokenId = _tokenIds.current();
+        _tokenIds.increment();
+        
+        tokenIdToFruit[newTokenId] = Fruit(
+          newTokenId,
+          data,
+          block.timestamp
+        );
+
+        return newTokenId;
+    }
+
+    function getFruit(uint256 tokenId) public view returns (Fruit memory) {
+      require(tokenId <= _tokenIds.current());
+      return tokenIdToFruit[tokenId];
+    }
+
+    function validateData(string memory data) private pure returns (bool) {
+      bytes memory b = bytes(data);
+      //41 -- 5A
+      for(uint i = 0; i<b.length; i++) {
+        bytes1 char = b[i];
+
+        if(!(char >= 0x30 && char <= 0x39)) {
+          if (!(char >= 0x41 && char <= 0x5A)){
+            return false;
+          }
+        }
+
+        if (i > 64) return false;
+      }
+
+      return true;
+    }
 }
