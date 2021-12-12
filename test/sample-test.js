d296e72da45145b35b43bc75ad509cee1d60ee1b
marcus <marcuseecl@gmail.com>
2021-12-12 17:02:44 +0800

    Add test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,6 +1,9 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const data1 = '1111111111111111111111111111111111111111111111111111111111111111';
+const data2 = '2222222222222222222222222222222222222222222222222222222222222222';
+
 describe("Bitfruit", function () {
   it("Should return the symbol when it is deployed", async function () {
     const Bitfruit = await ethers.getContractFactory("Bitfruit");
@@ -9,5 +12,15 @@ describe("Bitfruit", function () {
 
     const name = await bitfruit.name();
     expect(name).equal('BitFruit');
+  });
+
+  describe('Bit fruit creation' , async () => {
+    const Bitfruit = await ethers.getContractFactory("Bitfruit");
+    const bitfruit = await Bitfruit.deploy();
+    await bitfruit.createFruit(data1);
+    
+    const fruit = await bitfruit.getFruit(0);
+    
+    expect(fruit.data).equal(data1);
   });
 });
