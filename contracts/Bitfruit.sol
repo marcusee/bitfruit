f7fbcb8bb2fea0ad035cfd5bcb2cc42fe7f4566a
marcus <marcuseecl@gmail.com>
2021-12-15 23:38:22 +0800

    Add svg generation functions

--- contracts/Bitfruit.sol
+++ contracts/Bitfruit.sol
@@ -56,7 +56,12 @@ contract Bitfruit is ERC721URIStorage {
         );
 
         _mint(msg.sender, newTokenId);
-        
+        _setTokenURI(
+          newTokenId, 
+          formatTokenURI(
+            generateSVG(data)
+          )
+        );
         return newTokenId;
     }
 
@@ -120,6 +125,32 @@ contract Bitfruit is ERC721URIStorage {
         body,
         tail
       ));
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+      // example:
+      // <svg width='500' height='500' viewBox='0 0 285 350' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill='black' d='M150,0,L75,200,L225,200,Z'></path></svg>
+      // data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nNTAwJyBoZWlnaHQ9JzUwMCcgdmlld0JveD0nMCAwIDI4NSAzNTAnIGZpbGw9J25vbmUnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc+PHBhdGggZmlsbD0nYmxhY2snIGQ9J00xNTAsMCxMNzUsMjAwLEwyMjUsMjAwLFonPjwvcGF0aD48L3N2Zz4=
+      string memory baseURL = "data:image/svg+xml;base64,";
+      string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+      return string(abi.encodePacked(baseURL,svgBase64Encoded));
+    }
+
+    function formatTokenURI(string memory imageURI) public pure returns (string memory) {
+      return string(
+        abi.encodePacked(
+          "data:application/json;base64,",
+            Base64.encode(
+              bytes(
+                abi.encodePacked(
+                  '{"name":"',
+                  "SVG NFT", // You can add whatever name here
+                  '", "description":"An NFT based on SVG!", "attributes":"", "image":"',imageURI,'"}'
+                )
+              )
+            ) 
+        )
+      );
     }
 
     function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
